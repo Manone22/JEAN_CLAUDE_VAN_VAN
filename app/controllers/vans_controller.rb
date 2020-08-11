28c35797be68f7a8bf5f342416a63af1387f12ca
Manon de Demandolx dedons <manondedemandolx@hotmail.fr>
2020-08-11 11:30:27 +0200

    Vans done

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -1,28 +1,53 @@
 class VansController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show, :new, :edit, :create, :update, :destroy]
+  before_action :set_van, only: [:show, :edit, :update, :destroy]
 
   def index
     @vans = Van.all
   end
 
   def show
-    @van = Van.find(params[:id])
   end
 
   def new
     @van = Van.new
   end
 
+  def edit
+  end
+
   def create
     @van = Van.new(van_params)
-    if @van.save
-      redirect_to van_path(@van)
+    @van.user = current_user
+    if @van.save!
+      redirect_to @van, notice: 'Votre van a bien été enregistré!'
     else
-      render 'new'
+      render :new
     end
   end
 
+  def update
+    if @van.update(van_params)
+      redirect_to @van, notice: 'Votre van a bien été modifié!'
+    else
+      render :edit
+    end
+  end
+
+   def destroy
+    @van.destroy
+    redirect_to vans_url, notice: 'Votre van a bien été supprimé!'
+  end
+
+  private
+
+  def set_van
+    @van = Van.find(params[:id])
+  end
+
   def van_params
-    params.require(:van).permit(:description, :date, :price, :seat, :location, :image_url, :user)
+    params.require(:van).permit(:name, :description, :seat, :location, :date, :price, :image_url)
   end
 end
+
+
--- app/models/van.rb
+++ app/models/van.rb
@@ -1,4 +1,4 @@
 class Van < ApplicationRecord
   belongs_to :user
-  validates :name, :description, :date, :location, :price, :seat, :image_url, :user, presence: true
+  validates :name, :description, :seat, :location, :date, :price, :image_url, presence: true
 end
