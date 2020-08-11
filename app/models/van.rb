8079c0f81c89382c6475ab7f37cdf60987f066c0
Manon de Demandolx dedons <manondedemandolx@hotmail.fr>
2020-08-11 17:50:06 +0200

    savings

--- app/models/van.rb
+++ app/models/van.rb
@@ -2,4 +2,5 @@ class Van < ApplicationRecord
   belongs_to :user
   has_many :bookings
   validates :name, :description, :seat, :location, :date, :price, :image_url, presence: true
+  # à valider en groupe
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
-  resources :users do
-    resources :vans, only: [:index, :show, :create, :update] do
-      resources :bookings, only: [:index, :show, :create]
-    end
-  end
+  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :vans, only: [:new, :destroy, :edit]
-  resources :bookings, only: [:new, :destroy]
+  resources :vans do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:show, :destroy]
 end
