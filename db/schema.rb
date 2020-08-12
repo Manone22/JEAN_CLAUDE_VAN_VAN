4199f9df67b0e9365d104ff2b1a7e7acd27f2dc1
Robin Crochet <crochetrobin@gmail.com>
2020-08-12 11:08:20 +0200

    Addfooterhtml/css template

--- db/schema.rb
+++ db/schema.rb
@@ -14,17 +14,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_102915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  create_table "bookings", force: :cascade do |t|
-    t.date "date"
-    t.integer "price"
-    t.bigint "user_id", null: false
-    t.bigint "van_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_bookings_on_user_id"
-    t.index ["van_id"], name: "index_bookings_on_van_id"
-  end
-  
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -34,7 +24,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_102915) do
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
-  
+
   create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
@@ -46,7 +36,17 @@ ActiveRecord::Schema.define(version: 2020_08_11_102915) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  create_table "bookings", force: :cascade do |t|
+    t.date "date"
+    t.integer "price"
+    t.bigint "user_id", null: false
+    t.bigint "van_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+    t.index ["van_id"], name: "index_bookings_on_van_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_102915) do
     t.index ["user_id"], name: "index_vans_on_user_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "users"
   add_foreign_key "bookings", "vans"
   add_foreign_key "vans", "users"
