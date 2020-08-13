c348a5b612dd2c91a22d6cdfae77eb03b89b824f
Manon de Demandolx dedons <manondedemandolx@hotmail.fr>
2020-08-13 12:41:24 +0200

    pbm_seed_fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Van.destroy_all
-#puts "Destroying vans..."
+Van.destroy_all
+puts "Destroying vans..."
 
-Van.create!(seat: 5, name: "Vanlife1", description: "Super old school", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
-Van.create!(seat: 5, name: "Vanlife2", description: "Van completement rose", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
-Van.create!(seat: 5, name: "Vanlife3", description: "Le van de ta vie !!!", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
-Van.create!(seat: 5, name: "Vanlife4", description: "Super van", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
-Van.create!(seat: 5, name: "Vanlife5", description: "WonderWovan", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
-Van.create!(seat: 5, name: "Vanlife6", description: "Van verdoyant", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
-Van.create!(seat: 5, name: "Vanlife7", description: "Monster truck", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
-Van.create!(seat: 5, name: "Vanlife8", description: "VAn VROOM VROOM", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
+Van.create!(seat: 5, name: "Vanlife1", category:"volkswagen", description: "Super old school", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
+Van.create!(seat: 5, name: "Vanlife2", category:"volkswagen", description: "Van completement rose", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
+Van.create!(seat: 5, name: "Vanlife3", category:"volkswagen", description: "Le van de ta vie !!!", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
+Van.create!(seat: 5, name: "Vanlife4", category:"4x4", description: "Super van", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
+Van.create!(seat: 5, name: "Vanlife5", category:"4x4", description: "WonderWovan", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
+Van.create!(seat: 5, name: "Vanlife6", category:"4x4", description: "Van verdoyant", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
+Van.create!(seat: 5, name: "Vanlife7", category:"camper", description: "Monster truck", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
+Van.create!(seat: 5, name: "Vanlife8", category:"camper", description: "VAn VROOM VROOM", date: Date.today - rand(100).years, price: 340, location:"Marseille", image_url: "https://unsplash.com/photos/w8CcH9Md4vE", user: User.first)
 
 puts "Vans created !"
