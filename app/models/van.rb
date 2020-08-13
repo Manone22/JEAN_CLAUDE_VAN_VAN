f013cb37320ea123c8193f317383bf1b0670d8b6
Manon de Demandolx dedons <manondedemandolx@hotmail.fr>
2020-08-13 19:13:24 +0200

    active-storage-done/small-other-fixes

--- app/models/van.rb
+++ app/models/van.rb
@@ -1,7 +1,8 @@
 class Van < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  validates :name, :description, :seat, :location, :date, :price, :image_url, presence: true
+  has_one_attached :photo
+  validates :name, :description, :seat, :location, :date, :price, :photo, presence: true
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
