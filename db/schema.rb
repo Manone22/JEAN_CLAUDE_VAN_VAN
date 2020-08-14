b37a5a77092d2e54edde1d9730dd835341e9496a
Manon de Demandolx dedons <manondedemandolx@hotmail.fr>
2020-08-14 16:29:25 +0200

    savings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_150657) do
+ActiveRecord::Schema.define(version: 2020_08_14_142521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2020_08_13_150657) do
     t.string "category"
     t.float "latitude"
     t.float "longitude"
+    t.string "equipement"
     t.index ["user_id"], name: "index_vans_on_user_id"
   end
 
