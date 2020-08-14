5447d3206a3b868c059df7d1a8eb574079fff082
Robin Crochet <crochetrobin@gmail.com>
2020-08-14 12:17:21 +0200

    Integrated reservation booking specific start date and en date

--- app/models/van.rb
+++ app/models/van.rb
@@ -3,6 +3,12 @@ class Van < ApplicationRecord
   has_many :bookings, dependent: :destroy
   validates :name, :description, :seat, :location, :date, :price, :image_url, presence: true
 
+    def unavailable_dates
+      bookings.pluck(:start_date, :end_date).map do |range|
+        { from: range[0], to: range[1] }
+      end
+    end
+
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 
